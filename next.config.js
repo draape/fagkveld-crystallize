d0b5b88da315a852c9a98fe427e651d1b006a5a3
Erik Heumrønningen <erik.heumronningen@knowit.no>
2022-11-16 10:38:39 +0100

    Adds env var bindings to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 const { i18n } = require('./next-i18next.config');
 
 module.exports = {
+  env: {
+    env: {
+      NEXT_PUBLIC_CRYSTALLIZE_TENANT_IDENTIFIER:
+        process.env.NEXT_PUBLIC_CRYSTALLIZE_TENANT_IDENTIFIER,
+      NEXT_PUBLIC_SERVICE_API_URL: process.env.NEXT_PUBLIC_SERVICE_API_URL
+    }
+  },
   i18n,
   async redirects() {
     return [
